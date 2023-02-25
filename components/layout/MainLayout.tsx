e3a260bbc52e981ffa7913357b1b21b5180a3483
shahariar50 <shahariar.touhid@gmail.com>
2023-02-26 03:16:44 +0600

    setup navbar

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -1,5 +1,16 @@
-const MainLayout = () => {
-  return <div>MainLayout</div>;
+import Header from "./Header";
+
+type mainLayoutProps = {
+  children: React.ReactElement;
+};
+
+const MainLayout: React.FC<mainLayoutProps> = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
 };
 
 export default MainLayout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import MainLayout from "components/layout/MainLayout";
 import HeroSection from "components/pages/Homepage/HeroSection";
 
 export default function Home() {
   return (
-    <>
+    <MainLayout>
       <HeroSection />
-    </>
+    </MainLayout>
   );
 }
